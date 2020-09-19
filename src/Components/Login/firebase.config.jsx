77e269e15df30b4e81304f0e74230e39d74a25b3
kawsar00 <kawsarmahmud358@gmail.com>
2020-09-20 05:02:09 +0600

    site title name added

--- src/Components/Login/firebase.config.jsx
+++ src/Components/Login/firebase.config.jsx
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCtf9v0N9rCuaULTRiaLc9FigIvQdlTHmU",
-  authDomain: "travel-guru-eb965.firebaseapp.com",
-  databaseURL: "https://travel-guru-eb965.firebaseio.com",
-  projectId: "travel-guru-eb965",
-  storageBucket: "travel-guru-eb965.appspot.com",
-  messagingSenderId: "682926722304",
-  appId: "1:682926722304:web:1b20f4271cce822131f7e5"
+  apiKey: "AIzaSyA412isLfVPqxgj4X4WSSYF88b5djDoLWI",
+  authDomain: "travel-guru-apps-10a2c.firebaseapp.com",
+  databaseURL: "https://travel-guru-apps-10a2c.firebaseio.com",
+  projectId: "travel-guru-apps-10a2c",
+  storageBucket: "travel-guru-apps-10a2c.appspot.com",
+  messagingSenderId: "820421769602",
+  appId: "1:820421769602:web:a21d84fb74c7518578b79b"
 };
 
 export default firebaseConfig;
