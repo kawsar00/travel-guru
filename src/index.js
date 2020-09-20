dbc33c55e13492366b6668cfdaac359cd6025a4d
kawsar00 <kawsarmahmud358@gmail.com>
2020-09-20 18:27:36 +0600

    bootstrap file missing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-css/semantic.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
 
 ReactDOM.render(
